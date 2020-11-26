4c1575f4dd7fba4dff2749b657b7495735fef997
Benjamin68100 <61730304+Benjamin68100@users.noreply.github.com>
2020-11-26 16:44:31 +0100

    ajout modules

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Module;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
@@ -17,8 +18,12 @@ class ModuleController extends AbstractController
      */
     public function index(): Response
     {
+        $modules = $this->getDoctrine()
+            ->getRepository(Module::class)
+            ->getAll();
+
         return $this->render('module/index.html.twig', [
-            'controller_name' => 'ModuleController',
+            'modules' => $modules,
         ]);
     }
 }
