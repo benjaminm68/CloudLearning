e111576aaca574d42b224a7f647118b887df90b7
Benjamin68100 <61730304+Benjamin68100@users.noreply.github.com>
2020-12-04 16:43:48 +0100

    modif panel

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -8,15 +8,16 @@ use App\Entity\Session;
 use App\Entity\Categorie;
 use App\Entity\Formation;
 use App\Entity\Stagiaire;
+use App\Form\AddStagiaireType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 // Normalement on écrit adminController <----
-/**
-     * @Route("/panel", name="panel_index")
-     */
+
 
 class PanelController extends AbstractController
 {
@@ -24,7 +25,7 @@ class PanelController extends AbstractController
    
 
     /**
-     * @Route("/", name="panel_accueil")
+     * @Route("/panel", name="panel_index")
      */
     public function index(): Response
     {
@@ -62,4 +63,45 @@ class PanelController extends AbstractController
             'sessions' => $sessions
         ]);
     }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/panel/ajouter", name="panel_stagiaire_add")
+     * @Route("/panel/edit{id}", name="stagiaire_edit")
+     */
+    public function addEdit(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $manager)
+    {
+        if (!$stagiaire) {
+            $stagiaire = new Stagiaire();
+        }
+        $form = $this->createForm(AddStagiaireType::class, $stagiaire);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form->get('sessions')->getData();
+            $manager->persist($stagiaire);
+            $manager->flush();
+
+            return $this->redirectToRoute('stagiaire_index');
+        }
+        return $this->render('panel/panel-ajouterStagiaire.html.twig', [
+            'AddStagiaireType' => $form->createView(),
+            'editMode' => $stagiaire->getId() !== null,
+            'stagiaire' => $stagiaire->getNom()
+        ]);
+    }
+
+     /**
+     * @Route("/panel/ajouterStagiaire", name="panel_ajouterStagiaire")
+     */
+    // public function ajouterStagiaire(): Response
+    // {
+
+    //     $stagiaires = $this->getDoctrine()
+    //         ->getRepository(Stagiaire::class)
+    //         ->getAll();
+
+    //     return $this->render('stagiaire/index.html.twig', [
+    //         'stagiaires' => $stagiaires,
+    //     ]);
+    // }
 }
