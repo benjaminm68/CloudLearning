1c3fd9da262760145c21ded6ff64c60a5cf107aa
Benjamin68100 <61730304+Benjamin68100@users.noreply.github.com>
2020-12-08 15:06:38 +0100

    page d'accueil

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -39,6 +39,11 @@ class Categorie
      */
     private $modules;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -127,6 +132,18 @@ class Categorie
                 $module->setAppartenir(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
